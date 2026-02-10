6b75e77ca6b3e19dc2f88eb7682c892e713e9e17
Mahdi Shaker <mahdi.shaker92@gmail.com>
2026-02-10 09:15:50 +0100

    add majid name as profile

--- resources/views/sections/default/_topbar.blade.php
+++ resources/views/sections/default/_topbar.blade.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-top navbar-expand navbar-dashboard navbar-dark ps-0 pe-2 pb-0">
-    <div class="container-fluid px-0" style="background: #0b5ed7">
+<nav class="navbar navbar-top navbar-expand navbar-dashboard navbar-dark ps-0 pe-2 pb-0 pt-2 mt-2" style="background-color: #0b5ed7">
+    <div class="container-fluid px-0">
         <div class="d-flex justify-content-between w-100" id="navbarSupportedContent">
             <div class="d-flex align-items-center">
                 {{--        HERE WAS A SEARCH BOX - removing the -> the profile jumps to left        --}}
@@ -8,8 +8,8 @@
             <ul class="navbar-nav align-items-center">
                 <li class="nav-item dropdown ms-lg-3">
                     <a class="nav-link dropdown-toggle pt-1 px-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <div class="media d-flex align-items-center"><img class="avatar rounded-circle" alt="Image placeholder" src="{{asset('assets/img/team/profile-picture-3.jpg')}}">
-                            <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block"><span class="mb-0 font-small fw-bold text-gray-900">Bonnie Green</span></div>
+                        <div class="media d-flex align-items-center"><img class="avatar rounded-circle" alt="Image placeholder" src="{{asset('assets/img/team/dr-majid-taghvaei.png')}}">
+                            <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block"><span class="mb-0 font-small fw-bold text-accent-foreground">Dr. Majid Taghvaei</span></div>
                         </div>
                     </a>
                     <div class="dropdown-menu dashboard-dropdown dropdown-menu-end mt-2 py-1">
