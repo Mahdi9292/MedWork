7315bc9bb02988840d0885033ff044a0fd2f1f5c
Mahdi Shaker <mahdi.shaker92@gmail.com>
2026-02-23 13:22:22 +0100

    clean the blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-{{--    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">--}}
-{{--        <div class="d-block mb-3 mb-md-0">--}}
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+        <div class="d-block mb-3 mb-md-0">
 {{--            <x-template.breadcrumb :activePage="config('constants.APPLICATIONS.TWAP.TITLE')" />--}}
 {{--            <h2 class="h4">{{ config('constants.APPLICATIONS.TWAP.TITLE') }}</h2>--}}
 {{--            <p class="mb-0"></p>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+        </div>
+    </div>
 
     <div class="row">
         <x-template.landing-page-tile
@@ -16,7 +16,7 @@
             :version="'1.1'"
             :sub-title="__('neuen Bestelleingänge')"
             :short-info="__('Übersicht und Erfassung der Bestelleingänge zu neuen Maschinen')"
-            :permissions="['DEOrderBookAdmins']"
+            :permissions="['']"
             {{--            :version="config('constants.app_version.OrderBook')"--}}
         />
     </div>
