739e7d9efb4f9b2366d39c41ac0f43a14e7068a3
Mahdi Shaker <mahdi.shaker92@gmail.com>
2026-02-25 16:54:37 +0100

    activity - CRUD

--- resources/views/templates/medical/activity/create.blade.php
+++ resources/views/templates/medical/activity/create.blade.php
@@ -1,14 +1,14 @@
-@extends('templates.orderbook.base')
+@extends('layouts.app')
 
 @section('content')
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
         <div class="d-block mb-4 mb-md-0">
-            <x-template.breadcrumb :activePage="__('neuer Verantwortlicher')" :links="[
-                ['key' => config('constants.APPLICATIONS.ORDERBOOK.TITLE'), 'url' => url('orderbook')],
-                ['key' => __('Verantwortlicher'), 'url' => route('orderbook.responsibles.index')]
+            <x-template.breadcrumb :activePage="__('neuer Tätigkeit')" :links="[
+                ['key' => config('constants.APPLICATIONS.MEDICAL.TITLE'), 'url' => url('medical')],
+                ['key' => __('Tätigkeit'), 'url' => route('medical.activities.index')]
             ]" />
-            <h2 class="h4">{{ __('Verantwortlicher') }}</h2>
+            <h2 class="h4">{{ __('Tätigkeit Erstellen') }}</h2>
         </div>
     </div>
 
@@ -19,20 +19,12 @@
             <div class="card border-light shadow-sm components-section">
                 <div class="card-body">
                     <div class="col-lg-9 col-sm-12">
-                        <x-form.form :action="route('orderbook.responsibles.store')" novalidate hasJsValidation>
+                        <x-form.form :action="route('medical.activities.store')" novalidate hasJsValidation>
 
-                            <x-form.input name="lastname" :label="__('Nachname')"  required  />
-                            <x-form.input name="firstname" :label="__('Vorname')" required  />
-                            <x-form.input name="street" :label="__('Strasse')" required  />
-                            <x-form.input name="city" :label="__('Stadt')" required  />
-                            <x-form.input name="email" :label="__('E-mail')" required  />
-                            <x-form.input name="phone" :label="__('Telefon')" required  />
-                            <x-form.select name="region_id" :label="__('Region')" :options="$regions" required />
-                            <x-form.input name="postcode" :label="__('Postleitzahl')" required  />
-                            <x-form.input name="fax" :label="__('Fax')" />
-                            <x-form.checkbox name="active" :label="__('Aktiv')" :switch="true" checked/>
+                            <x-form.input name="name" :label="__('Tätigkeit')"  required  />
+                            <x-form.input name="code" :label="__('Code')" required  />
+
                             <button type="submit" class="btn btn-secondary">{{ __('Speichern') }}</button>
-
                         </x-form.form>
                     </div>
                 </div>
--- resources/views/templates/medical/activity/index.blade.php
+++ resources/views/templates/medical/activity/index.blade.php
@@ -1,27 +1,27 @@
-@extends('templates.orderbook.base')
+@extends('layouts.app')
 
 @section('content')
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
         <div class="d-block mb-4 mb-md-0">
-            <x-template.breadcrumb :activePage="__('Verantwortlicher')" :links="[['key' => config('constants.APPLICATIONS.ORDERBOOK.TITLE'), 'url' => url('orderbook')]]" />
-            <h2 class="h4">{{ __('Verantwortlicher') }}</h2>
+            <x-template.breadcrumb :activePage="__('Tätigkeit')" :links="[['key' => config('constants.APPLICATIONS.MEDICAL.TITLE'), 'url' => url('medical')]]" />
+            <h2 class="h4">{{ __('Tätigkeit') }}</h2>
             <p class="mb-0"></p>
         </div>
         <div class="btn-toolbar mb-2 mb-md-0">
-            @can(config('perm.orderbook.responsibles.create'))
+            @can(config('perm.medical.responsibles.create'))
                 <div class="btn-group">
-                    <a href="{{ route('orderbook.responsibles.create') }}" type="button" class="btn btn-sm btn-gray-800"><span class="fas fa-plus me-2"></span> {{ __('Neu') }}</a>
+                    <a href="{{ route('medical.activities.create') }}" type="button" class="btn btn-sm btn-gray-800"><span class="fas fa-plus me-2"></span> {{ __('Neu') }}</a>
                 </div>
             @endcan
             <div class="btn-group ms-2 ms-lg-3">
-                <button class="btn btn-sm btn-outline-primary" title="{{ __('alle verwendeten Filters zurücksetzen') }}" onclick="resetFilters('orderbook-responsible-table')"><i class="fas fa-times-circle"></i> {{ __('Filter') }}</button>
+                <button class="btn btn-sm btn-outline-primary" title="{{ __('alle verwendeten Filters zurücksetzen') }}" onclick="resetFilters('medical-activity-table')"><i class="fas fa-times-circle"></i> {{ __('Filter') }}</button>
             </div>
         </div>
     </div>
     <x-template.notification />
     <div class="card card-body shadow-sm table-wrapper table-responsive table-power-grid">
-        <livewire:order-book.data-tables.system.responsible-table />
+        <livewire:medical.activity-table />
     </div>
 
 @endsection
