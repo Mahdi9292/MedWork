efbd8665d0c512b8aa3e4bfbbe00a4c739326837
Mahdi Shaker <mahdi.shaker92@gmail.com>
2026-04-09 13:38:00 +0200

    adjust the certifiacate pdf

--- resources/views/templates/pdf/certificate_slip.blade.php
+++ resources/views/templates/pdf/certificate_slip.blade.php
@@ -25,35 +25,35 @@
 <div class="content">
     <div class="text-center mt-2"> {{ $isEmployer ? 'Arbeitgeber/in' : 'Arbeitnehmer/in' }}</div>
     <div class="title-section mt-4 mb-4">
-        <div class="font-size-15 fw-bold mb-1">Vorsorgebescheinigung</div>
+        <div class="font-size-15 fw-bold mb-1">{{__('Vorsorgebescheinigung')}}</div>
         <div class="font-size-11">
             nach § 6 Absatz 3 Nr. 3 der Verordnung zur arbeitsmedizinischen Vorsorge
         </div>
         <div class="font-size-9 text-end mt-2">
-            Bescheinigung Nr.: {{ $certificate->certificate_number }} <br>
+            {{__('Bescheinigung Nr.')}}: {{ $certificate->certificate_number }} <br>
             {{ __('Datum') }}: {{ formatDate($certificate->issue_date) }}
         </div>
 
     </div>
 
-    <div class="mb-4 font-size-10">
+    <div class="mb-4 font-size-9">
         {{ $certificate->employee_salutation ? ($certificate->employee_salutation == \App\Enums\Medical\SalutationType::ST_MR ? __('Proband') : __('Probandin')) : __('Proband/in') }}<br>
         <span class="fw-bold">{{$certificate->employee_salutation?->label()}} {{$certificate->employee_first_name}} {{$certificate->employee_last_name}}</span><br>
-        Geburtsdatum: {{ formatDate($certificate->employee_birthday) }}<br>
-        beschäftigt bei: {{$certificate->employer_name}}<br>
-        Anschrift des Arbeitgebers: {{$certificate->employer_street}} {{$certificate->employer_house_number}}, {{$certificate->employer_postcode}} {{$certificate->employer_city}}<br><br>
-        <span class="fw-bold">Untersuchungsdatum: {{ formatDate($certificate->examination_date) }}</span>
+        {{__('Geburtsdatum')}}: {{ formatDate($certificate->employee_birthday) }}<br>
+        {{__('beschäftigt bei')}}: {{$certificate->employer_name}}<br>
+        {{__('Anschrift des Arbeitgebers')}}: {{ $certificate->employer_address ?: $certificate->employer_street . ' ' . $certificate->employer_house_number . ', ' . $certificate->employer_postcode . ' ' . $certificate->employer_city}}<br><br>
+        <span class="fw-bold">{{__('Untersuchungsdatum')}}: {{ formatDate($certificate->examination_date) }}</span>
     </div>
 
-    <div class="mb-2 fw-bold font-size-10">Vorsorgen:</div>
+    <div class="mb-2 fw-bold font-size-10">{{__('Vorsorgen')}}:</div>
 
     <table class="table-border w-100p font-size-9">
         <thead>
         <tr>
-            <th class="w-5p p-1 text-center">Nr.</th>
-            <th class="p-1 text-start ps-2">Anlass / Tätigkeit</th>
-            <th class="p-1 text-center">Art der Vorsorge gemäß ArbMedVV</th>
-            <th class="w-20p p-1 text-center">Nächster Termin</th>
+            <th class="w-5p p-1 text-center">{{__('Nr.')}}</th>
+            <th class="p-1 text-start ps-2">{{__('Anlass/ Tätigkeit')}}</th>
+            <th class="p-1 text-center">{{__('Art der Vorsorge gemäß ArbMedVV')}}</th>
+            <th class="w-20p p-1 text-center">{{__('Nächster Termin')}}</th>
         </tr>
         </thead>
         <tbody>
@@ -68,9 +68,9 @@
         </tbody>
     </table>
 
-    <div class="mt-8 font-size-10">
-        <div class="fw-bold mb-1">Bemerkungen / Empfehlungen:</div>
-        <div style="text-align: justify;">
+    <div class="mt-8">
+        <div class="fw-bold mb-1">{{__('Bemerkungen/ Empfehlungen')}}:</div>
+        <div class="font-size-8" style="text-align: justify;">
             @if($isEmployer)
                 {{ $certificate->employer_comment_id ? $certificate->employerComment?->content : $certificate->employer_comment }}
             @else
@@ -79,25 +79,20 @@
         </div>
     </div>
 
-    <div class="mt-12 font-size-10">
-        <strong>{{ __('Ort') }}: Brake, {{ __('Datum') }}: {{ formatDate($certificate->issue_date) }}</strong>
-    </div>
+    <div class="mt-12 font-size-9" style="width: 100%; clear: both; overflow: hidden;">
 
-    <div class="mt-20">
-        <table class="w-100p border-0">
-            <tr>
-                <td class="w-30p"></td>
+        <div style="float: left; width: 50%; padding-top: 5px;">
+            <strong>
+                {{ __('Ort') }}: {{ $certificate->issue_location ?: __('Brake') }},
+                {{ __('Datum') }}: {{ formatDate($certificate->issue_date) }}
+            </strong>
+        </div>
 
-                <td class="w-70p text-center font-size-10 fw-bold" style="color: #2e5da7; white-space: nowrap;">
-                    <div class="mb-2">
-                        Unterschrift: Dr. med. Majid Taghvaei
-                    </div>
-                    <div>
-                        Facharzt für Arbeitsmedizin
-                    </div>
-                </td>
-            </tr>
-        </table>
+        <div style="float: right; width: 50%; text-align: center; color: #2e5da7; font-weight: bold; line-height: 1.2;">
+            <div>{{__('Unterschrift: Dr. med. Majid Taghvaei')}}</div>
+            <div>{{__('Facharzt für Arbeitsmedizin')}}</div>
+        </div>
+
     </div>
 </div>
 
